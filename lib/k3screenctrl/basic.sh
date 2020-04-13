3286399c429975b3d0ce18d2161bbe5fde43da7f
1005789164 <1005789164@qq.com>
2020-04-13 13:41:49 +0800

    Update basic.sh

--- lib/k3screenctrl/basic.sh
+++ lib/k3screenctrl/basic.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 . /etc/os-release
+. /etc/openwrt_release
 
-# PRODUCT_NAME_FULL=$(cat /etc/board.json | jsonfilter -e "@.model.name")
-# PRODUCT_NAME=${PRODUCT_NAME_FULL#* } # Remove first word to save space
-PRODUCT_NAME=${LEDE_DEVICE_PRODUCT}
+PRODUCT_NAME_FULL=$(cat /etc/board.json | jsonfilter -e "@.model.name")
+PRODUCT_NAME=${PRODUCT_NAME_FULL#* } # Remove first word to save space
+
+HW_VERSION="A1"
+
+FW_VERSION=${DISTRIB_REVISION}
 
 # WAN_IFNAME=$(uci get network.wan.ifname)
-MAC_ADDR=$(cat /tmp/k3screenctrl/macaddr)
+MAC_ADDR=$(ifconfig $WAN_IFNAME | grep -oE "([0-9A-Z]{2}:){5}[0-9A-Z]{2}")
 
-HW_VERSION=${LEDE_DEVICE_REVISION:0:2}
-FW_VERSION=$(cat /etc/openwrt_version)
-
-echo $PRODUCT_NAME
-
-if [ $(uci get k3screenctrl.general.cputemp) -eq 1 ]; then
+if [ $(uci get k3screenctrl.@general[0].cputemp) -eq 1 ]; then
     CPU_TEMP=$(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000))
-    echo $HW_VERSION $CPU_TEMP
-else
-    echo $HW_VERSION
+    LOAD=`uptime | awk -F 'average:' '{print$2}' | awk -F ',' '{print$1 $3}'`
+    HW_VERSION="T $CPU_TEMP *C"
+    MAC_ADDR="L$LOAD"
 fi
 
+echo $PRODUCT_NAME
+echo $HW_VERSION
 echo $FW_VERSION
 echo $MAC_ADDR
