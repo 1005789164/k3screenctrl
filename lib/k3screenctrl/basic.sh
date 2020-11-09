cab7163d5bc53553142c08212f9feaea9d2e3494
1005789164 <1005789164@qq.com>
2020-11-09 20:25:54 +0800

    Update basic.sh

--- lib/k3screenctrl/basic.sh
+++ lib/k3screenctrl/basic.sh
@@ -5,21 +5,29 @@
 PRODUCT_NAME_FULL=$(cat /etc/board.json | jsonfilter -e "@.model.name")
 PRODUCT_NAME=${PRODUCT_NAME_FULL#* } # Remove first word to save space
 
-HW_VERSION="A1"
-
-FW_VERSION=${DISTRIB_REVISION}
-
-# WAN_IFNAME=$(uci get network.wan.ifname)
+WAN_IFNAME=$(uci get network.wan.ifname)
 MAC_ADDR=$(ifconfig $WAN_IFNAME | grep -oE "([0-9A-Z]{2}:){5}[0-9A-Z]{2}")
 
-if [ $(uci get k3screenctrl.@general[0].cputemp) -eq 1 ]; then
-    CPU_TEMP=$(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000))
-    LOAD=`uptime | awk -F 'average:' '{print$2}' | awk -F ',' '{print$1 $3}'`
-    HW_VERSION="T $CPU_TEMP *C"
-    MAC_ADDR="L$LOAD"
-fi
+CPU_TEMP=$(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000))
+
+HW_VERSION="A1"
+#${LEDE_DEVICE_REVISION:0:2}
+FW_VERSION=${DISTRIB_REVISION:0:17}
 
 echo $PRODUCT_NAME
-echo $HW_VERSION
-echo $FW_VERSION
-echo $MAC_ADDR
+
+if [ $(uci get k3screenctrl.@general[0].showmore) -eq 1 ]; then
+    used=`free | grep Mem | awk '{print$3}'`
+    all=`free | grep Mem | awk '{print$2}'`
+    LOAD=`uptime | awk -F "average:" '{print$2}' | awk -F "," '{print$1}'`
+    UPTIME=`uptime | awk -F "," '{print$1}'|awk '{print"up " $3" " $4}'`
+    echo T: $CPU_TEMP*C
+    echo $UPTIME
+    echo U:$LOAD, R: $((100*$used/$all))%
+    echo $DISTRIB_DESCRIPTION
+else
+    echo $HW_VERSION
+    echo $FW_VERSION
+    echo $FW_VERSION
+    echo $MAC_ADDR
+fi
