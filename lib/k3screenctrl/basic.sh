98cc04bae1c9b472b6f7d24014b93d73bfe9e07b
1005789164 <1005789164@qq.com>
2020-11-13 10:19:54 +0800

    Update basic.sh

--- lib/k3screenctrl/basic.sh
+++ lib/k3screenctrl/basic.sh
@@ -8,26 +8,25 @@ PRODUCT_NAME=${PRODUCT_NAME_FULL#* } # Remove first word to save space
 WAN_IFNAME=$(uci get network.wan.ifname)
 MAC_ADDR=$(ifconfig $WAN_IFNAME | grep -oE "([0-9A-Z]{2}:){5}[0-9A-Z]{2}")
 
-CPU_TEMP=$(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000))
-
 HW_VERSION="A1"
 #${LEDE_DEVICE_REVISION:0:2}
 FW_VERSION=${DISTRIB_REVISION:0:17}
 
 echo $PRODUCT_NAME
 
-if [ $(uci get k3screenctrl.@general[0].showmore) -eq 1 ]; then
+if [ $(uci get k3screenctrl.@general[0].cputemp) -eq 1 ]; then
+    CPU_TEMP=$(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000))
+
     used=`free | grep Mem | awk '{print$3}'`
     all=`free | grep Mem | awk '{print$2}'`
     LOAD=`uptime | awk -F "average:" '{print$2}' | awk -F "," '{print$1}'`
-    UPTIME=`uptime | awk -F "," '{print$1}'|awk '{print"up " $3" " $4}'`
+    UPTIME=`uptime | awk -F "," '{print$1}' | awk '{print$3$4}'`
+
     echo T: $CPU_TEMP*C
-    echo $UPTIME
+    echo $FW_VERSION, $UPTIME
     echo U:$LOAD, R: $((100*$used/$all))%
-    echo $DISTRIB_DESCRIPTION
 else
     echo $HW_VERSION
-    echo $FW_VERSION
     echo $FW_VERSION
     echo $MAC_ADDR
 fi
